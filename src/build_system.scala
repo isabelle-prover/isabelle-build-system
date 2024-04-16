9cb9a48b1079ec46313e41bf666cea6a963d2a29
Fabian Huch <huch@in.tum.de>
2024-04-16 09:35:32 +0200

    add explicit poller;

--- src/build_system.scala
+++ src/build_system.scala
@@ -10,29 +10,39 @@ import scala.annotation.tailrec
 
 
 object Build_System {
-  def build_system(options: Options, progress: Progress = new Progress): Unit = {
-    val repository = Mercurial.self_repository()
-    val initial_id = repository.identify()
-    progress.echo("Starting build system on " + initial_id)
+  /* poller listening to repository updates */
 
-    @tailrec def loop(current_id: String): Unit = {
-      options.seconds("build_system_poll_delay").sleep()
-      
+  class Poller(
+    options: Options,
+    repository: Mercurial.Repository = Mercurial.self_repository(),
+    progress: Progress = new Progress
+  ) {
+    val initial_id = repository.id()
+    val poll_delay = options.seconds("build_system_poll_delay")
+
+    private def sleep(): Unit = poll_delay.sleep()
+
+    @tailrec private def poll(id: String): Unit = {
+      sleep()
+
       Exn.capture {
         repository.pull()
-        repository.identify()
+        repository.id()
       } match {
         case Exn.Exn(exn) =>
           progress.echo_warning("Could not poll repository: " + exn.getMessage)
-          loop(current_id)
-        case Exn.Res(id) if id != current_id =>
-          progress.echo("Found new revision: " + id)
-          loop(id)
-        case _ => loop(current_id)
+          poll(id)
+        case Exn.Res(id1) if id != id1 =>
+          progress.echo("Found new revision: " + id1)
+          poll(id1)
+        case _ => poll(id)
       }
     }
 
-    loop(initial_id)
+    def run(): Unit = {
+      progress.echo("Starting build system on " + initial_id)
+      poll(initial_id)
+    }
   }
 
 
@@ -57,7 +67,7 @@ Usage: isabelle build_system [OPTIONS]
 
       val progress = new Console_Progress()
 
-      build_system(options, progress)
+      new Poller(options, progress = progress).run()
     })
 }
 
