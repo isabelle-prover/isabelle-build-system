db35356ece8e208369d331a1ca85c703411c2812
Fabian Huch <huch@in.tum.de>
2024-04-16 09:04:39 +0200

    added repository polling loop;

--- src/build_system.scala
+++ src/build_system.scala
@@ -6,14 +6,38 @@ Isabelle system for automated and quasi-interactive build, with web frontend.
 package isabelle
 
 
+import scala.annotation.tailrec
+
+
 object Build_System {
   def build_system(options: Options, progress: Progress = new Progress): Unit = {
-    progress.echo("Hi")
+    val repository = Mercurial.self_repository()
+    val initial_id = repository.identify()
+    progress.echo("Starting build system on " + initial_id)
+
+    @tailrec def loop(current_id: String): Unit = {
+      options.seconds("build_system_poll_delay").sleep()
+      
+      Exn.capture {
+        repository.pull()
+        repository.identify()
+      } match {
+        case Exn.Exn(exn) =>
+          progress.echo_warning("Could not poll repository: " + exn.getMessage)
+          loop(current_id)
+        case Exn.Res(id) if id != current_id =>
+          progress.echo("Found new revision: " + id)
+          loop(id)
+        case _ => loop(current_id)
+      }
+    }
+
+    loop(initial_id)
   }
 
 
   /* Isabelle tool wrapper */
-  
+
   val isabelle_tool = Isabelle_Tool("build_system", "run build system", Scala_Project.here,
     { args =>
       var options = Options.init()
